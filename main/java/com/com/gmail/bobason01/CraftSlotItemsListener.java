c8247a348421da3f17aef3c520c6a0d1780d6895
Varus <bobason01@gmail.com>
2024-07-03 08:58:35 +0900

    코드 업데이트

--- main/java/com/com/gmail/bobason01/CraftSlotItemsListener.java
+++ main/java/com/com/gmail/bobason01/CraftSlotItemsListener.java
@@ -2,12 +2,14 @@ package com.gmail.bobason01;
 
 import com.destroystokyo.paper.event.player.PlayerRecipeBookClickEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerGameModeChangeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -42,37 +44,49 @@ public class CraftSlotItemsListener implements Listener {
 
     @EventHandler
     public void inventoryClick(InventoryClickEvent e) {
-        if (e.getWhoClicked().getOpenInventory().getType() == InventoryType.CRAFTING)
+        if (e.getWhoClicked().getOpenInventory().getType() == InventoryType.CRAFTING && e.getWhoClicked().getGameMode() != GameMode.CREATIVE)
             removeItems(e.getWhoClicked().getOpenInventory());
 
         Bukkit.getScheduler().runTaskLater(CraftSlotCommands.plugin, () -> {
             if (e.getWhoClicked().getOpenInventory().getTopInventory() instanceof CraftingInventory &&
-                    e.getWhoClicked().getOpenInventory().getType() == InventoryType.CRAFTING && !(e.getWhoClicked().isDead()))
+                    e.getWhoClicked().getOpenInventory().getType() == InventoryType.CRAFTING && e.getWhoClicked().getGameMode() != GameMode.CREATIVE && !(e.getWhoClicked().isDead()))
                 addItems(e.getWhoClicked().getOpenInventory());
         }, 1L);
     }
 
     @EventHandler
     public void inventoryClose(InventoryCloseEvent e) {
-        if (e.getPlayer().getOpenInventory().getType() == InventoryType.CRAFTING)
+        if (e.getPlayer().getOpenInventory().getType() == InventoryType.CRAFTING && e.getPlayer().getGameMode() != GameMode.CREATIVE)
             removeItems(e.getPlayer().getOpenInventory());
 
         Bukkit.getScheduler().runTaskLater(CraftSlotCommands.plugin, () -> {
             if (e.getPlayer().getOpenInventory().getTopInventory() instanceof CraftingInventory &&
-                    e.getPlayer().getOpenInventory().getType() == InventoryType.CRAFTING && !(e.getPlayer().isDead()))
+                    e.getPlayer().getOpenInventory().getType() == InventoryType.CRAFTING && e.getPlayer().getGameMode() != GameMode.CREATIVE && !(e.getPlayer().isDead()))
                 addItems(e.getPlayer().getOpenInventory());
+        }, 1L);
+    }
+
+    @EventHandler
+    public void inventoryOpen(InventoryOpenEvent e) {
+        if (e.getPlayer().getOpenInventory().getType() == InventoryType.CRAFTING && e.getPlayer().getGameMode() != GameMode.CREATIVE)
+            removeItems(e.getPlayer().getOpenInventory());
+
+        Bukkit.getScheduler().runTaskLater(CraftSlotCommands.plugin, () -> {
+            if (e.getPlayer().getOpenInventory().getTopInventory() instanceof CraftingInventory &&
+                    e.getPlayer().getOpenInventory().getType() == InventoryType.CRAFTING && e.getPlayer().getGameMode() != GameMode.CREATIVE && !(e.getPlayer().isDead()))
+                removeItems(e.getPlayer().getOpenInventory());
         }, 1L);
     }
 
     @EventHandler
     public void playerJoin(PlayerJoinEvent e) {
         if (e.getPlayer().getOpenInventory().getTopInventory() instanceof CraftingInventory &&
-                e.getPlayer().getOpenInventory().getType() == InventoryType.CRAFTING && !(e.getPlayer().isDead())) {
+                e.getPlayer().getOpenInventory().getType() == InventoryType.CRAFTING && e.getPlayer().getGameMode() != GameMode.CREATIVE && !(e.getPlayer().isDead())) {
             removeItems(e.getPlayer().getOpenInventory());
 
             Bukkit.getScheduler().runTaskLater(CraftSlotCommands.plugin, () -> {
                 if (e.getPlayer().getOpenInventory().getTopInventory() instanceof CraftingInventory &&
-                        e.getPlayer().getOpenInventory().getType() == InventoryType.CRAFTING && !(e.getPlayer().isDead()))
+                        e.getPlayer().getOpenInventory().getType() == InventoryType.CRAFTING && e.getPlayer().getGameMode() != GameMode.CREATIVE && !(e.getPlayer().isDead()))
                     addItems(e.getPlayer().getOpenInventory());
             }, 1L);
         }
@@ -81,12 +95,12 @@ public class CraftSlotItemsListener implements Listener {
     @EventHandler
     public void playerLeave(PlayerQuitEvent e) {
         if (e.getPlayer().getOpenInventory().getTopInventory() instanceof CraftingInventory &&
-                e.getPlayer().getOpenInventory().getType() == InventoryType.CRAFTING) {
+                e.getPlayer().getOpenInventory().getType() == InventoryType.CRAFTING && e.getPlayer().getGameMode() != GameMode.CREATIVE) {
             removeItems(e.getPlayer().getOpenInventory());
 
             Bukkit.getScheduler().runTaskLater(CraftSlotCommands.plugin, () -> {
                 if (e.getPlayer().getOpenInventory().getTopInventory() instanceof CraftingInventory &&
-                        e.getPlayer().getOpenInventory().getType() == InventoryType.CRAFTING)
+                        e.getPlayer().getOpenInventory().getType() == InventoryType.CRAFTING && e.getPlayer().getGameMode() != GameMode.CREATIVE)
                     removeItems(e.getPlayer().getOpenInventory());
             }, 1L);
         }
@@ -97,12 +111,12 @@ public class CraftSlotItemsListener implements Listener {
     @EventHandler
     public void playerDeath(PlayerDeathEvent e) {
         if (e.getEntity().getOpenInventory().getTopInventory() instanceof CraftingInventory &&
-                e.getEntity().getOpenInventory().getType() == InventoryType.CRAFTING)
+                e.getEntity().getOpenInventory().getType() == InventoryType.CRAFTING && e.getEntity().getGameMode() != GameMode.CREATIVE)
             removeItems(e.getEntity().getOpenInventory());
 
         Bukkit.getScheduler().runTaskLater(CraftSlotCommands.plugin, () -> {
             if (e.getEntity().getOpenInventory().getTopInventory() instanceof CraftingInventory &&
-                    e.getEntity().getOpenInventory().getType() == InventoryType.CRAFTING)
+                    e.getEntity().getOpenInventory().getType() == InventoryType.CRAFTING && e.getEntity().getGameMode() != GameMode.CREATIVE)
                 removeItems(e.getEntity().getOpenInventory());
         }, 1L);
     }
@@ -110,12 +124,12 @@ public class CraftSlotItemsListener implements Listener {
     @EventHandler
     public void playerRespawn(PlayerRespawnEvent e) {
         if (e.getPlayer().getOpenInventory().getTopInventory() instanceof CraftingInventory &&
-                e.getPlayer().getOpenInventory().getType() == InventoryType.CRAFTING)
+                e.getPlayer().getOpenInventory().getType() == InventoryType.CRAFTING && e.getPlayer().getGameMode() != GameMode.CREATIVE)
             removeItems(e.getPlayer().getOpenInventory());
 
         Bukkit.getScheduler().runTaskLater(CraftSlotCommands.plugin, () -> {
             if (e.getPlayer().getOpenInventory().getTopInventory() instanceof CraftingInventory &&
-                    e.getPlayer().getOpenInventory().getType() == InventoryType.CRAFTING && !(e.getPlayer().isDead()))
+                    e.getPlayer().getOpenInventory().getType() == InventoryType.CRAFTING && e.getPlayer().getGameMode() != GameMode.CREATIVE && !(e.getPlayer().isDead()))
                 addItems(e.getPlayer().getOpenInventory());
         }, 1L);
     }
@@ -134,18 +148,18 @@ public class CraftSlotItemsListener implements Listener {
     }
 
     private void addItems(InventoryView inventory) {
-        if(inventory.getItem(1) == null || Objects.requireNonNull(inventory.getItem(1)).getType().isAir())
+        if(inventory.getItem(1) == null || Objects.requireNonNull(inventory.getItem(1)).getType() == Material.AIR)
             inventory.setItem(1, i1);
-        if(inventory.getItem(2) == null || Objects.requireNonNull(inventory.getItem(2)).getType().isAir())
+        if(inventory.getItem(2) == null || Objects.requireNonNull(inventory.getItem(2)).getType() == Material.AIR)
             inventory.setItem(2, i2);
-        if(inventory.getItem(3) == null || Objects.requireNonNull(inventory.getItem(3)).getType().isAir())
+        if(inventory.getItem(3) == null || Objects.requireNonNull(inventory.getItem(3)).getType() == Material.AIR)
             inventory.setItem(3, i3);
-        if(inventory.getItem(4) == null || Objects.requireNonNull(inventory.getItem(4)).getType().isAir())
+        if(inventory.getItem(4) == null || Objects.requireNonNull(inventory.getItem(4)).getType() == Material.AIR)
             inventory.setItem(4, i4);
-        if(inventory.getItem(1) != null && !(Objects.requireNonNull(inventory.getItem(1)).getType().isAir()) &&
-                inventory.getItem(2) != null && !(Objects.requireNonNull(inventory.getItem(2)).getType().isAir() &&
-                inventory.getItem(3) != null && !(Objects.requireNonNull(inventory.getItem(3)).getType().isAir()) &&
-                inventory.getItem(4) != null && !(Objects.requireNonNull(inventory.getItem(4)).getType().isAir())))
+        if(inventory.getItem(1) != null && !(Objects.requireNonNull(inventory.getItem(1)).getType() == Material.AIR) &&
+                inventory.getItem(2) != null && !(Objects.requireNonNull(inventory.getItem(2)).getType() == Material.AIR) &&
+                inventory.getItem(3) != null && !(Objects.requireNonNull(inventory.getItem(3)).getType() == Material.AIR) &&
+                inventory.getItem(4) != null && !(Objects.requireNonNull(inventory.getItem(4)).getType() == Material.AIR))
             inventory.setItem(0, i0);
     }
     static void removeItems(InventoryView inventory) {
